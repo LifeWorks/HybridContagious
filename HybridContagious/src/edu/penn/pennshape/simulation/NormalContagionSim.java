68deb7fb3aa6184b361b48b51b1c8fa1ec520489
LifeWorks <LifeWorks.Song@gmail.com>
2018-05-01 16:15:15 -0600

    add parallel stream for random contagion and normal contagion

--- HybridContagious/src/edu/penn/pennshape/simulation/NormalContagionSim.java
+++ HybridContagious/src/edu/penn/pennshape/simulation/NormalContagionSim.java
@@ -1,25 +1,36 @@
 package edu.penn.pennshape.simulation;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Arrays;
 
 public class NormalContagionSim extends NetworkSim {
 	public static void main(String[] args) {
 
 		NormalContagionSim ns = new NormalContagionSim();
 
-		int threshold = 5;
 		int numofcommunityseeds = 1;
 		int numofmessageseeds = 1;
+		double threshold = 5;
 		double mean = 2;
 		double std = 0.0;
 		
-		for (int i=0;i<7;i++) {
-			for (int j=0;j<10;j++) {
-				mean = 2 + i;
-				std = 0.5 + j * 0.5;
-				ns.simNormalContigions(threshold, numofcommunityseeds,
-						numofmessageseeds, mean, std);
+		List<ArrayList<Double>> combinations = new LinkedList< ArrayList<Double> >();
+		for (int h=0;h<11;h++) {
+			for (int i=2;i<9;i++) {
+				for (int j=0;j<10;j++) {
+					if (i != 5 || h != 5) {
+						threshold = h;
+						mean = i;
+						std = 0.5 + j * 0.5;
+						combinations.add(new ArrayList<Double>(Arrays.asList(threshold,mean,std)));
+					}
+				}
 			}
 		}
+		
+		combinations.parallelStream().forEach(combo -> ns.simNormalContigions(combo.get(0).intValue(), numofcommunityseeds, numofmessageseeds, combo.get(1), combo.get(2))); 
+		
 	}
 }
--- HybridContagious/src/edu/penn/pennshape/simulation/RandomContagionSim.java
+++ HybridContagious/src/edu/penn/pennshape/simulation/RandomContagionSim.java
@@ -1,20 +1,34 @@
 package edu.penn.pennshape.simulation;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Arrays;
 
 public class RandomContagionSim extends NetworkSim {
 	public static void main(String[] args) {
 
 		RandomContagionSim ns = new RandomContagionSim();
 
-		int threshold = 5;
-		int numofcommunityseeds = 0;
+		int numofcommunityseeds = 1;
 		int numofmessageseeds = 1;
+		double threshold = 5;
 		double mean = 5;
 		double std = 0.0;
 		
-		ns.simRandomContigions(threshold, numofcommunityseeds,
-				numofmessageseeds, mean, std);
+		List<ArrayList<Double>> combinations = new LinkedList< ArrayList<Double> >();
+		for (int h=0;h<11;h++) {
+			for (int i=2;i<9;i++) {
+				for (int j=0;j<10;j++) {
+					threshold = h;
+					mean = i;
+					std = 0.5 + j * 0.5;
+					combinations.add(new ArrayList<Double>(Arrays.asList(threshold,mean,std)));
+				}
+			}
+		}
+
+		combinations.parallelStream().forEach(combo -> ns.simRandomContigions(combo.get(0).intValue(), numofcommunityseeds, numofmessageseeds, combo.get(1), combo.get(2))); 
 
 	}
 }
