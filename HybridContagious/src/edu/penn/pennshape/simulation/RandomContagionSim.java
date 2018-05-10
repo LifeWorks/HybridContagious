a1aaceeef20e24fcc94945b01cd5dfd743f33938
LifeWorks <LifeWorks.Song@gmail.com>
2018-05-10 12:11:19 -0600

    update direct contagion

--- HybridContagious/src/edu/penn/pennshape/simulation/RandomContagionSim.java
+++ HybridContagious/src/edu/penn/pennshape/simulation/RandomContagionSim.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Arrays;
+import java.lang.Math;
 
 public class RandomContagionSim extends NetworkSim {
 	public static void main(String[] args) {
@@ -18,11 +19,11 @@ public class RandomContagionSim extends NetworkSim {
 		
 		List<ArrayList<Double>> combinations = new LinkedList< ArrayList<Double> >();
 		for (int h=0;h<11;h++) {
-			for (int i=2;i<9;i++) {
-				for (int j=0;j<10;j++) {
+			for (int i=1;i<10;i++) {
+				for (int j=0;j<7;j++) {
 					threshold = h;
-					mean = i;
-					std = 0.5 + j * 0.5;
+					mean = i * 0.1;
+					std = 0.01 * Math.pow(2, j);
 					combinations.add(new ArrayList<Double>(Arrays.asList(threshold,mean,std)));
 				}
 			}
