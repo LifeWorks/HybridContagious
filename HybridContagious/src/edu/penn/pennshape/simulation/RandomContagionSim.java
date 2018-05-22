78d2075b7f2fe4d1bf8fcbc499ee47664b350ea2
LifeWorks <LifeWorks.Song@gmail.com>
2018-05-22 10:18:34 -0600

    change sd of direct contagion

--- HybridContagious/src/edu/penn/pennshape/simulation/RandomContagionSim.java
+++ HybridContagious/src/edu/penn/pennshape/simulation/RandomContagionSim.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Arrays;
-import java.lang.Math;
+//import java.lang.Math;
 
 public class RandomContagionSim extends NetworkSim {
 	public static void main(String[] args) {
@@ -20,10 +20,10 @@ public class RandomContagionSim extends NetworkSim {
 		List<ArrayList<Double>> combinations = new LinkedList< ArrayList<Double> >();
 		for (int h=0;h<11;h++) {
 			for (int i=1;i<10;i++) {
-				for (int j=0;j<7;j++) {
+				for (int j=1;j<8;j++) {
 					threshold = h;
 					mean = i * 0.1;
-					std = 0.01 * Math.pow(2, j);
+					std = 0.1 * j;
 					combinations.add(new ArrayList<Double>(Arrays.asList(threshold,mean,std)));
 				}
 			}
