160fee94c201e6bd03a083423b4047967a376b5d
LifeWorks <LifeWorks.Song@gmail.com>
2018-05-22 10:14:29 -0600

    Changed a bit

--- HybridContagious/src/edu/penn/pennshape/simulation/NormalContagionSim2.java
+++ HybridContagious/src/edu/penn/pennshape/simulation/NormalContagionSim2.java
@@ -1,6 +1,6 @@
 package edu.penn.pennshape.simulation;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class NormalContagionSim2 extends NetworkSim {
 	public static void main(String[] args) {
